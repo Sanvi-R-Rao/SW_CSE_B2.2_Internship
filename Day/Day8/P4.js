b7ee1e8c32a08f119b084245eb1b49fa08fa57ed
Sanvi-R-Rao <sanvirrao861@gmail.com>
2026-08-12 13:02:36 +0530

    Operators in JS-Arithmetic & Assignment operators

---
+++ Day/Day8/P4.js
@@ -0,0 +1,25 @@
+// Operators in JS
+// Arithmetic operators
+console.log("Arithmetic operators")
+let a = 10;
+let b = 3;
+console.log(a + b); // 13
+console.log(a - b); // 7
+console.log(a * b); // 30
+console.log(a / b); // 3.333...
+console.log(a % b); // 1
+console.log(a ** b); // 1000
+
+// Assignment operators
+console.log("Assignment operators")
+let x = 5;
+x += 2; // x = x + 2
+console.log(x)
+x -= 1; // x = x - 1
+console.log(x)
+x *= 3; // x = x * 3
+console.log(x)
+x /= 2; // x = x / 2
+console.log(x)
+x %= 2; // x = x % 2
+console.log(x)
