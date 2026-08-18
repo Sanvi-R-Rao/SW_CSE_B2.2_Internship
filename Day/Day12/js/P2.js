7a8d2f316e2db79545b3c21f1bc9132eb5c0b74e
Sanvi-R-Rao <sanvirrao861@gmail.com>
2026-08-18 12:21:16 +0530

    named export & default export

---
+++ Day/Day12/js/P2.js
@@ -0,0 +1,6 @@
+import { add,subtract } from "./P1.js"; //named export
+import multiply from "./P3.js"; //default export
+
+console.log(add(40,60));
+console.log(subtract(10,7));
+console.log(multiply(5,10));
