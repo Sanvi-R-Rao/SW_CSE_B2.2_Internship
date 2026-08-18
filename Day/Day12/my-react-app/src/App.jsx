973fff41ddf864a8266b5bdc844a55f91fc8c56f
Sanvi-R-Rao <sanvirrao861@gmail.com>
2026-08-18 13:01:39 +0530

    Functional component| default export

--- Day/Day12/my-react-app/src/App.jsx
+++ Day/Day12/my-react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import { Welcome } from "./components/P1.jsx";
+import Greet from "./components/P2.jsx"
 function App() {
   return (
     <div>
       <Welcome/>
+      <Greet/>
     </div>
-  )
+  );
 }
 
 export default App
---
+++ Day/Day12/my-react-app/src/components/P2.jsx
@@ -0,0 +1,8 @@
+export default function Greet() {
+    return(
+        <>
+        <h6>Hello,My name is Sanvi</h6>
+        <p>I'm learning React Js now.</p>
+        </>
+    )
+}
