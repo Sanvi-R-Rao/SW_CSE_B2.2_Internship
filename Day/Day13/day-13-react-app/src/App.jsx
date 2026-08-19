c3be1506135919da8f4f3fe094cc6430769184bd
Sanvi-R-Rao <sanvirrao861@gmail.com>
2026-08-19 12:00:30 +0530

    React State eg1

--- Day/Day13/day-13-react-app/src/App.jsx
+++ Day/Day13/day-13-react-app/src/App.jsx
@@ -2,10 +2,12 @@ import {Welcome} from "./components/P1";
 import {ProductCard} from "./components/P2";
 import './App.css';
 import ButtonDemo from "./components/P3";
+import Counter from "./components/StateExample1.jsx";
 function App() {
   return (
     <>
     <Welcome name="Sanvi"/>
+    <Counter/>
     <ButtonDemo />
     <ProductCard
     // name="Sony WH-CH720N Active Noise Cancellation Wireless Bluetooth Over Ear Headphones"
