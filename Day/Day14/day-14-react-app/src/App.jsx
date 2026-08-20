cc848cbdfa65a6e5fa9c23749639839c6f5599bf
Sanvi-R-Rao <sanvirrao861@gmail.com>
2026-08-20 10:45:15 +0530

    Event Handler eg2

--- Day/Day14/day-14-react-app/src/App.jsx
+++ Day/Day14/day-14-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import ClickDemo from "./components/EventHandler1.jsx";
+import FormDemo from "./components/EventHandler2.jsx";
 function App() {
 
   return (
     <>
     <ClickDemo/>
+    <FormDemo/>
     </>
   )
 }
 
-export default App
+export default App;
