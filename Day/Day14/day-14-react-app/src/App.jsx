844e46e8413d656eac18d994c14224971851e3f6
Sanvi-R-Rao <sanvirrao861@gmail.com>
2026-08-20 11:33:53 +0530

    List and Keys eg 1

--- Day/Day14/day-14-react-app/src/App.jsx
+++ Day/Day14/day-14-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import ClickDemo from "./components/EventHandler1.jsx";
 import FormDemo from "./components/EventHandler2.jsx";
 import ActionButtonDemo from "./components/EventHandler3.jsx";
+import SimpleList from "./components/ListAndKeys1.jsx";
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
     <ClickDemo/>
     <FormDemo/>
     <ActionButtonDemo/>
+    <SimpleList/>
     </>
   )
 }
